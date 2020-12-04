1c56e9195fecfa233c9920dbe2395d55f58e5697
robbaile <robbaile12@outlook.com>
2020-12-04 13:14:54 +0000

    Master - initial commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const AWS = require('aws-sdk');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.post('/email', (req, res) => {
+    AWS.config.update({
+        accessKeyId: process.env.AWS_ACCESSKEYID,
+        secretAccessKey: process.env.AWS_SECRETACCESSKEY,
+        region: process.env.AWS_REGION
+    });
+
+    const docClient = new AWS.DynamoDB();
+
+    console.log(`email: ${req.body.email}`);
+
+    const params = {
+        TableName: process.env.AWS_EMAIL_TABLE_NAME,
+        Item: {
+            email : { 
+                S: req.body.email 
+            }
+        }
+    };
+
+    docClient.putItem(params, function(err, data) {
+        if (err) {
+            console.log(err);
+            res.send({
+                success: false,
+                message: 'Error: Server error'
+            });
+        } else {
+            res.send({
+                success: true,
+                message: 'Saved blog',
+                blogs: data
+            });
+        }
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
